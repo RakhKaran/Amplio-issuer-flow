2331dd8869bd6d0f62869f4cd46415c55a02633b
Pratik813335 <girasepratiksing@gmail.com>
2026-02-16 16:18:13 +0530

    table name fixed

--- src/sections/signatories/view/signatories-list-view.js
+++ src/sections/signatories/view/signatories-list-view.js
@@ -54,8 +54,8 @@ const STATUS_OPTIONS = [{ value: 'all', label: 'All' }];
 
 const TABLE_HEAD = [
   { id: 'fullName', label: 'Name' },
-  { id: 'roleValue', label: 'Role' },
   { id: 'email', label: 'Email' },
+  { id: 'roleValue', label: 'Role' },
   { id: 'phone', label: 'Phone' },
   { id: 'time', label: 'Status' },
   { id: '', label: 'Actions' },
@@ -77,8 +77,8 @@ export default function SignatoriesListView() {
 
 
 
-  const {Signatories}= useGetSignatories();
-console.log(Signatories);
+  const { Signatories } = useGetSignatories();
+  console.log(Signatories);
 
   const handleViewRow = useCallback(
     (id) => {
@@ -87,7 +87,7 @@ console.log(Signatories);
     [router]
   );
 
-   const handleEditRow = useCallback(
+  const handleEditRow = useCallback(
     (id) => {
       router.push(paths.dashboard.signatories.edit(id));
     },
